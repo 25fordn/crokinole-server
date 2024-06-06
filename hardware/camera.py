89e7a47e64fb747bd84f836801b949c402fa7b35
Noah Ford <25fordn@gmail.com>
2024-06-06 14:53:35 -0400

    streaming works

--- hardware/camera.py
+++ hardware/camera.py
@@ -3,10 +3,10 @@ import cv2
 
 
 class Camera:
-    def __init__(self, n=0, frame_rate=30, target_res=(1920, 1080)):
-        self.cam = cv2.VideoCapture(n)
+    def __init__(self, camera_number=0, frame_rate=30, target_res=(1920, 1080)):
+        self.cam = cv2.VideoCapture(camera_number)
         if not self.cam.isOpened():
-            raise ValueError(f"Failed to open camera with number {n}")
+            raise ValueError(f"Failed to open camera with number {camera_number}")
         self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, target_res[0])
         self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, target_res[1])
         self.res = int(self.cam.get(cv2.CAP_PROP_FRAME_WIDTH)), int(self.cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -14,27 +14,39 @@ class Camera:
         self.prev_time = 0
         self.prev_frame = None
 
-    def get_frame(self):
+    def capture_frame(self):
         curr_time = time.time()
         if curr_time - self.prev_time < 1 / self.frame_rate:
             return self.prev_frame
-        ret, img = self.cam.read()
+        ret, frame = self.cam.read()
         if not ret:
             return self.prev_frame
         self.prev_time = curr_time
-        self.prev_frame = img
-        return img
+        self.prev_frame = frame
+        return frame
 
     def close(self):
         self.cam.release()
 
 
+def convert(frame, file_format=".jpg"):
+    return cv2.imencode(file_format, frame)[1].tobytes()
+
+
+def initialize_cameras() -> dict[str, Camera]:
+    cams = {'overhead': Camera(camera_number=0, frame_rate=30, target_res=(1920, 1080)),
+            'onboard': Camera(camera_number=0, frame_rate=30, target_res=(1920, 1080))}
+    return cams
+
+
+cameras = initialize_cameras()
+
+
 def main():
-    cam = Camera(n=0, frame_rate=30, target_res=(50, 50))
-    print(cam.res)
+    cam = cameras['overhead']
     while True:
-        img = cam.get_frame()
-        cv2.imshow("Camera", img)
+        img = cam.capture_frame()
+        cv2.imshow("Overhead Camera", img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     cam.close()
--- server.py
+++ server.py
@@ -1,11 +1,24 @@
+import base64
+import time
+from threading import Lock
+
+import eventlet.wsgi
+eventlet.monkey_patch()
+
 from flask import Flask
 from flask_cors import CORS
+from flask_socketio import SocketIO
+
+from hardware.camera import Camera, cameras, convert
 
 
 class Server:
     def __init__(self):
         self.app = Flask(__name__)
         CORS(self.app, resources={r"/*": {"origins": "*"}})
+        self.socketio = SocketIO(self.app, cors_allowed_origins="*")
+        self.streams = {}
+        self.lock = Lock()
         self.add_routes()
 
     def add_routes(self):
@@ -13,10 +26,56 @@ class Server:
         def serve():
             return {"message": "This is the server speaking!"}, 200
 
+        @self.app.route('/capture/<camera_name>')
+        def capture(camera_name):
+            cam = cameras[camera_name] if camera_name in cameras else None
+            if not cam:
+                return {"message": "Invalid camera name"}, 400
+            frame = cam.capture_frame()
+            frame = convert(frame, file_format=".jpg")
+            return frame, 200
+
+        @self.socketio.on('connect')
+        def handle_connect():
+            print('Client connected')
+
+        @self.socketio.on('disconnect')
+        def handle_disconnect():
+            print('Client disconnected')
+
+        @self.app.route('/stream/<camera_name>')
+        def stream(camera_name):
+            cam = cameras[camera_name] if camera_name in cameras else None
+            if not cam:
+                return {"error": "Invalid camera name"}, 400
+
+            with self.lock:
+                if camera_name not in self.streams:
+                    self.streams[camera_name] = self.socketio.start_background_task(self.stream_video, cam, camera_name)
+                    return {"message": f"Started streaming video feed for camera \"{camera_name}\""}, 200
+                else:
+                    return {"message": f"Already streaming video feed for camera \"{camera_name}\""}, 200
+
+    def stream_video(self, cam: Camera, camera_name: str):
+        last = time.time()
+        while True:
+            frame = cam.capture_frame()
+            frame = convert(frame, file_format=".jpg")
+            frame = base64.b64encode(frame).decode('utf-8')
+            self.socketio.emit('video_feed', {'camera': camera_name, 'frame': frame})
+            curr = time.time()
+            if curr - last >= 3:
+                print(f"Streaming video feed for camera \"{camera_name}\"")
+                last = curr
+            with self.lock:
+                if camera_name not in self.streams:
+                    break
+            eventlet.sleep()
+
     def run(self, port=None, debug=False):
-        self.app.run(port=port, debug=debug)
+        self.socketio.run(self.app, port=port, debug=debug, log_output=debug, use_reloader=debug)
 
 
 if __name__ == '__main__':
     server = Server()
-    server.run(debug=True)
+    server.run(debug=False)
