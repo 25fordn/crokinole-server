ccee84382308d2d5eb95ea9f883381cb4060b711
Noah Ford <25fordn@gmail.com>
2024-06-06 19:06:29 -0400

    added live feed

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import time
 from threading import Lock
 
 import eventlet.wsgi
+
 eventlet.monkey_patch()
 
 from flask import Flask
@@ -15,8 +16,8 @@ from hardware.camera import Camera, cameras, convert
 class Server:
     def __init__(self):
         self.app = Flask(__name__)
-        CORS(self.app, resources={r"/*": {"origins": "*"}})
-        self.socketio = SocketIO(self.app, cors_allowed_origins="*")
+        CORS(self.app, origins='*')
+        self.socketio = SocketIO(self.app, cors_allowed_origins='*')
         self.streams = {}
         self.lock = Lock()
         self.add_routes()
@@ -33,15 +34,19 @@ class Server:
                 return {"message": "Invalid camera name"}, 400
             frame = cam.capture_frame()
             frame = convert(frame, file_format=".jpg")
+            frame = base64.b64encode(frame).decode('utf-8')
             return frame, 200
 
-        @self.socketio.on('connect')
-        def handle_connect():
-            print('Client connected')
-
-        @self.socketio.on('disconnect')
-        def handle_disconnect():
-            print('Client disconnected')
+        @self.socketio.on('streamViewerConfirm')
+        def handle_viewer_confirm(data):
+            try:
+                camera_name = data['camera']
+                if camera_name not in self.streams:
+                    return
+                with self.lock:
+                    self.streams[camera_name]['lastConfirmedTime'] = time.time()
+            except KeyError as e:
+                return
 
         @self.app.route('/stream/<camera_name>')
         def stream(camera_name):
@@ -51,28 +56,41 @@ class Server:
 
             with self.lock:
                 if camera_name not in self.streams:
-                    self.streams[camera_name] = self.socketio.start_background_task(self.stream_video, cam, camera_name)
+                    self.streams[camera_name] = {
+                        'thread': self.socketio.start_background_task(self.stream_video, cam, camera_name),
+                        'lastConfirmedTime': time.time(),
+                    }
                     return {"message": f"Started streaming video feed for camera \"{camera_name}\""}, 200
                 else:
                     return {"message": f"Already streaming video feed for camera \"{camera_name}\""}, 200
 
     def stream_video(self, cam: Camera, camera_name: str):
-        last = time.time()
+        request_frequency = 10
+        request_timeout = 30
+        last_request_time = time.time()
+
+        print(f"Starting stream for camera \"{camera_name}\"")
+
         while True:
             frame = cam.capture_frame()
             frame = convert(frame, file_format=".jpg")
             frame = base64.b64encode(frame).decode('utf-8')
             self.socketio.emit('video_feed', {'camera': camera_name, 'frame': frame})
-            curr = time.time()
-            if curr - last >= 3:
-                print(f"Streaming video feed for camera \"{camera_name}\"")
-                last = curr
+
+            if time.time() - last_request_time > request_frequency:
+                self.socketio.emit('streamViewerCheck', {'camera': camera_name})
+                last_request_time = time.time()
+
             with self.lock:
-                if camera_name not in self.streams:
+                last_confirmed_time = self.streams[camera_name]['lastConfirmedTime']
+                if camera_name not in self.streams or time.time() - last_confirmed_time > request_timeout:
+                    print(f"Stopping stream for camera \"{camera_name}\"")
+                    self.streams.pop(camera_name)
                     break
             eventlet.sleep()
 
     def run(self, port=None, debug=False):
+        print("Starting server...")
         self.socketio.run(self.app, port=port, debug=debug, log_output=debug, use_reloader=debug)
 
 
